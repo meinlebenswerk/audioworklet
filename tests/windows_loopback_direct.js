f4f3cfec63448140a84594e81068a6ea0fabd8bf
Jan Eckert <jan.eckert@tum.de>
2020-12-03 23:36:14 +0100

    added "direct" loopback test

---
+++ tests/windows_loopback_direct.js
@@ -0,0 +1,26 @@
+const { AudioServer } = require('../');
+const audioServer = new AudioServer();
+
+const main = async () => {
+  const outDevice = audioServer.getDefaultOutputDevice();
+  console.log(`Recording from ${outDevice.name}`);
+
+  const CHANNELS = 2;
+  const SAMPLE_RATE = 48000;
+
+  const inputStream = audioServer.initInputStream(outDevice.id, {
+    sampleRate: SAMPLE_RATE,
+    format: AudioServer.F32LE,
+    channels: CHANNELS,
+  }, true);
+
+  inputStream.registerAudioCallback((data) => console.log(data))
+
+  inputStream.start();
+
+  setTimeout(() => {
+    inputStream.stop();
+  }, 10000);
+}
+
+main();
